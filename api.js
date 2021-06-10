65da84294234d3ce009572de2549a7d318d50757
ellsonmendesyt <elsonmendesoficial@gmail.com>
2021-06-10 07:52:34 -0300

    setup inicial

---
+++ api.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+app.use(express.json())
+app.use(cors())
+
+
+
+
+app.listen(3001,()=>{
+    console.log('seridor ativo ...');
+})
+
+
